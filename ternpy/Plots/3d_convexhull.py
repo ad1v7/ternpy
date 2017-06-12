aa66e553ff2265caeb602bfb0a77902ee7719c0c
ad1v7 <marcin.kirsz@gmail.com>
2017-06-12 22:41:08 +0100

    added convex hull and load_data functions

--- ternpy/Plots/3d_convexhull.py
+++ ternpy/Plots/3d_convexhull.py
@@ -7,21 +7,49 @@ import math
 from scipy.spatial import ConvexHull
 from matplotlib.tri import Triangulation
 
-# generate 3D convex hull for a given file
-def convex_hull_3d(input_file):
-    try:
-        X = np.genfromtxt(input_file, delimiter="", skip_header=2)
-        return ConvexHull(X)
 
-    except ValueError:
-        print(os.path.basename(input_file) +
-              " Oops! Looks like wrong file format...")
+# returns array containg data for ConvexHull()
+def load_data(file_list):
+    data = []
+    pressures = []
+    for entry in file_list:
+        try:
+            data.append(np.genfromtxt(entry, delimiter="", skip_header=2))
+            with open(entry, 'r') as f:
+                press = f.readline().split()[3]
+                f.close()
+            pressures.append(press)
+        except ValueError:
+            print(os.path.basename(entry) +
+                  " Oops! Looks like wrong file format...")
+    return data, pressures
 
 
-# returns array containing 3D convex hull objects
-def get_all_convex_hulls(files):
-    convex_hulls = []
-    for file in files:
-        convex_hulls.append(convex_hull_3d(file))
 
-convex_hull_3d(sys.argv[1])
+# returns array containing convex hull objects
+def get_all_convex_hulls(data):
+    hulls = []
+    for entry in data:
+        hulls.append(ConvexHull(entry))
+    return hulls
+
+
+# returns the value of the lowest point (z coordinate)
+# from the convex hull list
+def min_z(data, hulls):
+    minz = 0.
+    for idx, hull in enumerate(hulls):
+        if minz > min(data[idx][hull.vertices, 2]):
+            minz = min(data[idx][hull.vertices, 2])
+    return minz
+
+if __name__ == '__main__':
+    print len(sys.argv)
+    #convex_hull_3d(sys.argv[1])
+    #print(len(get_all_convex_hulls(load_data(sys.argv[1:]))))
+    data, pressures = load_data(sys.argv[1:])
+    chulls = get_all_convex_hulls(data)
+    #print(data[chulls.vertices, 2])
+    print(min_z(data, chulls))
+    print len(sys.argv)
+    print pressures
